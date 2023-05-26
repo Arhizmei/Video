bcf0f0ece64d41e2af3cc83a1a613dbe01841619
arhizmei <arhizmei@gmail.com>
2023-05-26 19:54:26 +0300

    initiateAndFinish

---
+++ app/src/main/java/com/zmei/videoapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.zmei.videoapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String VIDEO_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        Uri videoUri = Uri.parse(VIDEO_URL);
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+    }
+}
